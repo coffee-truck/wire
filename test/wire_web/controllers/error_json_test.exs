249a3ef55c1c5da96765ffbfa00604e43815494a
Shawn <shawn@tawk.to>
2024-02-27 20:57:17 +0800

    Initial commit

---
+++ test/wire_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule WireWeb.ErrorJSONTest do
+  use WireWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert WireWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert WireWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
